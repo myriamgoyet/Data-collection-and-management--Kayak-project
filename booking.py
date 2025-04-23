c5943bb7976f2789f3c70927d92405c7292ef428
myriamgoyet <myriam.goyet@hotmail.fr>
2025-04-23 16:02:28 +0200

    scraping hotels

--- booking.py
+++ booking.py
@@ -24,14 +24,12 @@ class Hotel_Spider(scrapy.Spider):
     def parse(self,response):
         hotel_name = response.css("div.f6431b446c.a15b38c233 ::text")
         hotel_url = response.css("a.a78ca197d0 ::attr(href)")
-        hotel_rating = response.css('div.a3b8729ab1.d86cee9b25 ::text')
 
-        for hotel_name, hotel_url, hotel_rating in zip (hotel_name, hotel_url, hotel_rating):
+        for hotel_name, hotel_url in zip (hotel_name, hotel_url):
             yield {
-                'city':urllib.parse.unquote(response.url[response.url.find('ss=')+len('ss='):]),
+                'city' : urllib.parse.unquote(response.url[response.url.find('ss=')+len('ss='):]),
                 'hotel_name' : hotel_name.get(),
                 'hotel_url': hotel_url.get(),
-                'hotel_rating':[rating for rating in response.css('div.a3b8729ab1.d86cee9b25 ::text').get() if "Avec une note de " not in rating]
             }
         
 # Name of the file where the results will be saved
@@ -39,17 +37,21 @@ filename = "booking_hotels.json"
 
 #If file already exists, delete it before crawling (because Scrapy will 
 #concatenate the last and new results otherwise)
-if filename in os.listdir('folder/'):
-        os.remove('folder/' + filename)
+if filename in os.listdir('data/'):
+        os.remove('data/' + filename)
 
+# Declare a new CrawlerProcess with some settings
+## USER_AGENT => Simulates a browser on an OS
+## LOG_LEVEL => Minimal Level of Log 
+## FEEDS => Where the file will be stored 
 process = CrawlerProcess(settings = {
     'USER_AGENT': 'Chrome/106.0.5249.62',
     'LOG_LEVEL': logging.INFO,
     "FEEDS": {
-        'folder/' + filename : {"format": "json"},
+        'data/' + filename : {"format": "json"},
     }
 })
 
-process.crawl(booking_spider)
+process.crawl(Hotel_Spider)
 process.start()
 
